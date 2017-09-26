ce78bb4d774070c26725dbbbc331565e590939a4
SamuelBWasserman <sbw61@man.cs.rutgers.edu>
2017-09-26 19:09:45 -0400

    Change the arrows

--- sorter.c
+++ sorter.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include "sorter.h"
 #include <string.h>
+#include "sorter.h"
 
 int main(int argc, char **(argv)){
-	if (argc < 2 || argc > 2) {
-		printf("invalid input\n");
+	// Quit if # of arguments are incorrect
+	if (argc < 3 || argc > 3) {
+		printf("invalid arguments\n");
     		return 0;
   	}
-	FILE *data_file = fopen(arv[1],"r");
+	
+	// Initialize empty row that stdin will populate
+	data_row *row = (data_row*) malloc(sizeof(data_row));
+	// extremely long while loop that assings everything to everything	
+	while(fscanf(stdin,"%s %s %d %d %d %d %s %d %d %s %s %s %d %d %s %d %s %s %d %s %s %s %f %d %d %f %f %d", row->color, row.director_name, row.num_critic_for_reviews, row.duration, row.director_facebook_likes, row.actor_3_facebook_likes, row.actor_2_name, row.actor_1_facebook_likes, row.gross, row.genres, row.actor_1_name, row.movie_title, row.num_voted_users, row.total_facebook_likes, row.actor_3_name, row.facenumber_in_poster, row.plot_keywords, row.movie_imbd_link, row.num_user_for_reviews, row.language, row.country, row.content_rating, row.budget, row.title_year, row.actor_2_facebook_likes, row.imdb_score, row.aspect_ratio, row.movie_facebook_likes ) != -1){
+		print(row->language);
+	
+		
+	}	
 }
 
 
--- sorter.h
+++ sorter.h
@@ -1,14 +1,12 @@
 // For the movie data set I counted 28 columns, but for a general CSV,
 // I would need the # of columns as meta data.
-const int NUMBER_OF_COLUMNS = 28;
-//Suggestion: define a struct that mirrors a record (row) of the data set
+// Suggestion: define a struct that mirrors a record (row) of the data set
 typedef struct data_row{
-  //I figure we can track all of the values as integers for sorting.
-  int column_values[NUMBER_OF_COLUMNS];
+  // I figure we can track all of the values as integers for sorting.
   char * color;
   char * director_name;
   int num_critic_for_reviews;
-  float duration; //Because I don't know how to make dateTime obj
+  float duration; // Because I don't know how to make dateTime obj
   int director_facebook_likes;
   int actor_3_facebook_likes;
   char * actor_2_name;
@@ -37,5 +35,5 @@ typedef struct data_row{
 }data_row;
 
 // Suggestion: prototype a mergesort function
-// Merge sort can take in the column number that needs to be sorted and return a new sorted array of rows. 
-*data_row mergesort(int);
+// Merge sort can take in the column number that needs to be sorted and return a new sorted array of rows.
+void mergesort(data_row **,char *);
