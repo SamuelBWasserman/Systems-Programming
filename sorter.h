122decd737631e7a088ce5b631af84fc646cb828
Ram Patel <rr7patel@gmail.com>
2017-09-28 22:26:00 -0400

    fuck you erlich bachman

---
+++ sorter.h
@@ -0,0 +1,44 @@
+// For the movie data set I counted 28 columns, but for a general CSV,
+// I would need the # of columns as meta data.
+// Suggestion: define a struct that mirrors a record (row) of the data set
+typedef struct data_rows {
+  // I figure we can track all of the values as integers for sorting.
+  char *col[28];
+
+} data_row;
+
+enum column {
+  color,
+  director_name,
+  num_critic_for_reviews,
+  duration,
+  director_facebook_likes,
+  actor_3_facebook_likes,
+  actor_2_name,
+  actor_1_facebook_likes,
+  gross,
+  genres,
+  actor_1_name,
+  movie_title,
+  num_voted_users,
+  cast_total_facebook_likes,
+  actor_3_name,
+  facenumber_in_poster,
+  plot_keywords,
+  movie_imdb_link,
+  num_user_for_reviews,
+  language,
+  country,
+  content_rating,
+  budget,
+  title_year,
+  actor_2_facebook_likes,
+  imdb_score,
+  aspect_ratio,
+  movie_facebook_likes
+};
+
+// Suggestion: prototype a mergesort function
+// Merge sort can take in the column number that needs to be sorted and return a
+// new sorted array of rows.
+void mergesort(data_row **, char *, enum column);
