7aa76defadf9235c4f90f9abc77c727ce18f08d0
SamuelBWasserman <sbw61@man.cs.rutgers.edu>
2017-09-29 08:35:14 -0400

    Chose which column to sort

--- sorter.c
+++ sorter.c
@@ -21,21 +21,82 @@ int main(int argc, char **(argv)){
 		word = strtok(line,",");
 		// Tokenize until end of line
 	    	while(word != NULL){
-			// The movie column has commas in between quotes but needs to be stores as one token
-			if(strpbrk(word,"\"") != NULL){
-				char tmp1[100];
-				strcpy(tmp1,word);
-				char *tmp = tmp1 + 1;
-				word = strtok(NULL,"\"");
-				strcat(tmp,word);
-				strcpy(word,tmp);
-			}
-			db[line_counter].col[word_counter] = (char *) malloc(strlen(word) * sizeof(char));
-			db[line_counter].col[word_counter] = word;
-                	word = strtok(NULL,",");
-                	word_counter++;
-            	}
-            	word_counter = 0;
-            	line_counter++;
-	}
+			    // The movie column has commas in between quotes but needs to be stores as one token
+			    if(strpbrk(word,"\"") != NULL){
+				    char tmp1[100];
+				    strcpy(tmp1,word);
+				    char *tmp = tmp1 + 1;
+				    word = strtok(NULL,"\"");
+				    strcat(tmp,word);
+				    strcpy(word,tmp);
+			    }
+			    // Allocate enough space for the string to be placed in the array
+			    db[line_counter].col[word_counter] = (char *) malloc(strlen(word) * sizeof(char));
+			    // Copy the string into the array
+			    strcpy(db[line_counter].col[word_counter],word);
+			    //Move to the next token
+                word = strtok(NULL,",");
+                word_counter++;
+            	    }
+            	    word_counter = 0;
+            	    line_counter++;
+	    }
+	    int column_to_sort; // will be passed to merge sort
+	        if(strcmp(argv[2],"color") == 0)
+	            column_to_sort = 1;
+	        else if(strcmp(argv[2],"director_name") == 0)
+	            column_to_sort = 2;
+	        else if (strcmp(argv[2],"num_critic_for_reviews") == 0)
+	            column_to_sort = 3;
+	        else if (strcmp(argv[2],"duration") == 0)
+	            column_to_sort = 4;
+	        else if (strcmp(argv[2],"director_facebook_likes") == 0)
+	            column_to_sort = 5;
+	        else if (strcmp(argv[2],"actor_3_facebook_likes") == 0)
+	            column_to_sort = 6;
+	        else if (strcmp(argv[2],"actor_2_name") == 0)
+	            column_to_sort = 7;
+	        else if (strcmp(argv[2],"actor_1_facebook_likes") == 0)
+	            column_to_sort = 8;
+	        else if (strcmp(argv[2],"gross") == 0)
+	            column_to_sort = 9;
+	        else if (strcmp(argv[2],"genres") == 0)
+	            column_to_sort = 10;
+	        else if (strcmp(argv[2],"actor_1_name") == 0)
+	            column_to_sort = 11;
+	        else if (strcmp(argv[2],"movie_title") == 0)
+	            column_to_sort = 12;
+	        else if (strcmp(argv[2],"num_voted_users") == 0)
+	            column_to_sort = 13;
+	        else if (strcmp(argv[2],"cast_total_facebook_likes") == 0)
+	            column_to_sort = 14;
+	        else if (strcmp(argv[2],"actor_3_name") == 0)
+	            column_to_sort = 15;
+	        else if (strcmp(argv[2],"facenumber_in_poster") == 0)
+	            column_to_sort = 16;
+	        else if (strcmp(argv[2],"plot_keywords") == 0)
+	            column_to_sort = 17;
+	        else if (strcmp(argv[2],"movie_imdb_link") == 0)
+	            column_to_sort = 18;
+	        else if (strcmp(argv[2],"num_user_for_reviews") == 0)
+	            column_to_sort = 19;
+	        else if (strcmp(argv[2],"language") == 0)
+	            column_to_sort = 20;
+	        else if (strcmp(argv[2],"country") == 0)
+	            column_to_sort = 21;
+	        else if (strcmp(argv[2],"content_rating") == 0)
+	            column_to_sort = 22;
+	        else if (strcmp(argv[2],"budget") == 0)
+	            column_to_sort = 23;
+	        else if (strcmp(argv[2],"title_year") == 0)
+	            column_to_sort = 24;
+	        else if (strcmp(argv[2],"actor_2_facebook_likes") == 0)
+	            column_to_sort = 25;
+	        else if (strcmp(argv[2],"imdb_score") == 0)
+	            column_to_sort = 26;
+	        else if (strcmp(argv[2],"aspect_ration") == 0)
+	            column_to_sort = 27;
+	        else if (strcmp(argv[2],"movie_facebook_likes") == 0)
+	            column_to_sort = 28;
+	    // Call merge sort with db and column_to_sort
 }
