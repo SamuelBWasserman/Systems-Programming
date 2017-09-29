c03ea1960fd67836aeff4ccaa98edb7a22a75b57
SamuelBWasserman <sbw61@man.cs.rutgers.edu>
2017-09-28 23:30:40 -0400

    Naw

--- sorter.c
+++ sorter.c
@@ -10,30 +10,36 @@ int main(int argc, char **(argv)){
     		return 0;
   	}
 	data_row db[5100]; // enough space to store every row
-	data_row *row = (data_row*)malloc(sizeof(data_row)); // Initialize empty data row
 	fscanf(stdin, "%*[^\n]"); // Read and discard a line
-    char line[400]; // one line from the file
-    char first_line[400];
-    fgets(first_line,400,stdin);// discard first line
-    int line_counter = 0; // count what line we're on to keep track of the struct array
-    int word_counter = 0; // keep track of what word were on for assignment in the struct
+	char line[400]; // one line from the file
+    	char first_line[400];
+    	fgets(first_line,400,stdin);// discard first line
+    	int line_counter = 0; // count what line we're on to keep track of the struct array
+    	int word_counter = 0; // keep track of what word were on for assignment in the struct
 	while(fgets(line,400,stdin) != NULL){
-            char *word;
-            word = strtok(line,",");
-            while(word != NULL){
-		if(strpbrk(word,"\"") != NULL){
-			char tmp1[100];
-			strcpy(tmp1,word);
-			char *tmp = tmp1 + 1;
-			word = strtok(NULL,"\"");
-			strcat(tmp,word);
-			strcpy(word,tmp);
-		}
-	    }
-                word = strtok(NULL,",");
-                word_counter++;
-            }
-            word_counter = 0;
-            line_counter++;
+		char *word;
+            	data_row *row = (data_row*)malloc(sizeof(data_row)); // Initialize empty data row
+		word = strtok(line,",");
+	    	char *col[28]
+		// Tokenize until end of line
+	    	while(word != NULL){
+			// The movie column has commas in between quotes but needs to be stores as one token
+			if(strpbrk(word,"\"") != NULL){
+				char tmp1[100];
+				strcpy(tmp1,word);
+				char *tmp = tmp1 + 1;
+				word = strtok(NULL,"\"");
+				strcat(tmp,word);
+				strcpy(word,tmp);
+			}
+			// Assign value to the row
+	    		col[word_counter] = word;	
+                	word = strtok(NULL,",");
+                	word_counter++;
+            	}
+		data_row -> col = col;
+		db[line_counter] = data_row;
+            	word_counter = 0;
+            	line_counter++;
 	}
 }
